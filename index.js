f4f75d350bd2aa198b336ba48ea8181a6b5dfae0
Pauli Partanen <pauli.tapio.partanen@outlook.com>
2021-01-20 15:22:12 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-
+require('dotenv').config()
 const { response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const Person = require('./models/person')
 
 
 /*Postman valitti
@@ -21,59 +22,40 @@ person: morgan.token("person", (req, res) => {
 });
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :person"))
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-
 app.get('/api/persons', (req, res) => {
-    //console.log('persons')
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
 })
 
 app.get('/info', (req, res) => {
-    //console.log('info')
-    res.send(`
-    <div>
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>
-    </div>`)
+    Person.countDocuments()
+        .then(response => {
+            res.send(`<div>
+        <p>Phonebook has info for ${response} people</p>
+        <p>${Date()}</p>
+        </div>`)
+        })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    //console.log('id')
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person) {
+                res.json(person)
+            } else {
+                res.status(404).end()
+            }
+        }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-    res.status(204).end()
+
+    Person.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
@@ -85,32 +67,53 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
-    if (persons.find(person => person.name === body.name)) {
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+    })
+
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number
     }
 
-    const person = body
-    person.id = Math.floor(Math.random() * 1000)
-    //console.log(person)
-
-    persons = persons.concat(person)
-
-    //console.log(persons)
-
-    res.json(person)
-
-
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+}
+
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
-
-/* ReactDOM.render(
-    <App />,-
-    document.getElementById('root')
-  ) */
 
